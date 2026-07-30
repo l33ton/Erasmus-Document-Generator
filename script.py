cede535eb4bc6652cb7f908ab973942f0c552034
l33ton <deividadarov05@gmail.com>
2026-07-30 18:07:25 +0300

    sss

--- script.py
+++ script.py
@@ -1,27 +1,69 @@
 import datetime
+import sys
 from pathlib import Path
 import pandas as pd
 from docxtpl import DocxTemplate
 
 base_dir = Path(__file__).parent
 
-acceptance_letter_template_path = base_dir / "Acceptance_Letter_Template_Blank.docx"
+if getattr(sys, 'frozen', False):
+    base_dir = Path(sys.executable).parent
+else:
+    base_dir = Path(__file__).parent
+
 nominations = base_dir / "Erasmus_Nominations_Data.xlsx"
-output_dir = base_dir / "generated letters"
+output_dir = base_dir / "generated_letters"
 
-output_dir.mkdir(exist_ok=True)
+class AcceptanceLetterGenerator:
+    def __init__(self, template_path):
+        self.template_path = template_path       
 
-doc = DocxTemplate(acceptance_letter_template_path)
-data_sheet = pd.read_excel(nominations, sheet_name="Sheet1")
+    def generate(self, record, template_path, student_folder):    
+        doc = DocxTemplate(template_path)
+        doc.render(record)
+        file_name = f"{record['FullName']}'s Acceptance Letter.docx"
+        doc.save(student_folder / file_name)
 
-data_sheet['StartDate'] = pd.to_datetime(data_sheet['StartDate']).dt.strftime('%d.%m.%Y')
-data_sheet['EndDate'] = pd.to_datetime(data_sheet['EndDate']).dt.strftime('%d.%m.%Y')
+class AccommodationLetterGenerator:
+    def __init__(self, template_path):
+        self.template_path = template_path
 
-for record in data_sheet.to_dict(orient="records"):
-    doc = DocxTemplate(acceptance_letter_template_path)
-    doc.render(record) # type: ignore
+    def generate(self, record, template_path, student_folder):    
+        doc = DocxTemplate(template_path)
+        doc.render(record)
+        file_name = f"{record['FullName']}'s Accommodation Letter.docx"
+        doc.save(student_folder / file_name)
 
-    file_name = f"{record['FullName']}'s Acceptance Letter.docx"
-    output_path = output_dir / file_name
+class GrantAgreementGenerator:
+    def __init__(self, template_path):
+        self.template_path = template_path
 
-    doc.save(output_path)
+    def generate(self, record, template_path, student_folder):    
+        doc = DocxTemplate(template_path)
+        doc.render(record)
+        file_name = f"{record['FullName']}'s Grant Agreement.docx"
+        doc.save(student_folder / file_name)
+
+
+def main():
+    if not nominations.exists:
+        return f"Nominations does not exist{nominations.name}"
+
+    acceptance_letter_template = AcceptanceLetterGenerator(base_dir / 'AcceptanceLetterTemplate.docx')
+    accommodation_letter_template = AcceptanceLetterGenerator(base_dir / 'AccommodationLetterTemplate.docx')
+    grant_agreement_template = GrantAgreementGenerator(base_dir / 'GrantAgreementTemplate.docx')
+
+    template_reader = pd.read_excel(nominations, sheet_name="Sheet1")
+    template_reader['StartDate'] = pd.to_datetime(template_reader['StartDate']).dt.strftime('%d.%m.%Y')
+    template_reader['EndDate'] = pd.to_datetime(template_reader['EndDate']).dt.strftime('%d.%m.%Y')
+
+    for record in template_reader.to_dict(orient="records"):
+        student_folder = output_dir / f"{record['FullName']}"
+        student_folder.mkdir(exist_ok=True)
+
+        acceptance_letter_template.generate(record, acceptance_letter_template, student_folder)
+        accommodation_letter_template.generate(record, accommodation_letter_template, student_folder)
+        grant_agreement_template.generate(record, grant_agreement_template, student_folder)
+
+if __name__ == "__main__":
+    main()        
